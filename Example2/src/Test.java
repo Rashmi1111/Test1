1605294427337ebabbaab2514679af937e0756c4
rashmi.c.kumari <rashmi.c.kumari@accenture.com>
2015-09-08 17:00:24 +0530

    fourth change

--- Example2/src/Test.java
+++ Example2/src/Test.java
@@ -10,6 +10,12 @@ public class Test {
 	    for(int i= 0;i<5;i++){
 	    	System.out.println(i);
 	    }
+	    
+	    for(int j= 0;j<5;j++){
+	    	System.out.println(j+1);
+	    }
+	    
+	    
 	
 
 	}
